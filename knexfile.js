d8b8324889d5dea6dccf11db259830c7628d8f1d
RayC-prog <71979738+RayC-prog@users.noreply.github.com>
2020-12-14 13:49:35 -0300

    configurando a estrutura do projeto

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,14 @@
-const dotenv = require ('dotenv')
+const dotenv = require('dotenv')
 dotenv.config()
 
-
-const knex = require ('knex')({
-    client: 'mysql2',
-    connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
-  }
+const knex = require('knex')({
+    client: 'mysql2' ,
+    connection:{
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE
+    }
 })
 
-module.exports = knex
-
-
-
+module.exports = knex 
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-//importar o express
+//Importar as dependencias
 const express = require('express')
 const bodyParser = require('body-parser')
+const moment = require('moment')
 
-//importando as rotas do aviso
-const routerAvisos = require('./components/avisos/AvisosController')
+//Importando as rotas do aviso
+const routerAvisos = require('./components/Avisos/AvisosController')
+const router = require('./components/Avisos/AvisosController')
 
-//inicilizar o express
+//Iniciando o express
 const app = express()
 
-//configurar a view engine e a pasta publica
+//Configuração da view engine e a pasta pública
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
-//configurar o body parser
+moment.locale('pt-br')
+app.locals.moment = moment
+
+//Configuração do body-parser
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-//rotas
+
+//Rotas
 app.use(routerAvisos)
 
-//porta 
+//escutando a porta 3000
 app.listen(3000)
+
